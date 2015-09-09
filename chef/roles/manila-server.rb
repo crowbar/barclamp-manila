fba2a9a5402a9f2e0b45cb422deda04a08d04289
Vincent Untz <vuntz@suse.com>
2015-09-09 12:14:26 +0200

    Add note that this got moved to merged repo

--- chef/roles/manila-server.rb
+++
@@ -1,8 +0,0 @@
-name "manila-server"
-description "Manila API and Scheduler Role"
-run_list(
-  "recipe[manila::api]",
-  "recipe[manila::scheduler]"
-)
-default_attributes
-override_attributes
--- chef/roles/manila-share.rb
+++
@@ -1,7 +0,0 @@
-name "manila-share"
-description "Manila share Role"
-run_list(
-  "recipe[manila::share]"
-)
-default_attributes
-override_attributes
